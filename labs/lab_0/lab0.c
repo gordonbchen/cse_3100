72dd140bc703c380f3ee3cd1e3e1069c1924cd1b
Gordon Chen <107230749+gordonbchen@users.noreply.github.com>
2025-01-24 19:43:55 +0000

    add lab0

---
+++ labs/lab_0/lab0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    int sum = 0;
+    int i = 2;
+    while (i < 200) {
+        sum += i;
+        i += 2;
+    }
+    printf("%d\n", sum);
+   
+     return 0;
+}
+
