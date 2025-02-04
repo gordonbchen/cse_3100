3660ffc074b7b9c5a02d241911d2afcf07244292
gordonbchen <gordonbchen@gmail.com>
2025-02-04 16:06:25 -0500

    fix happy whitespaces

--- hw/hw_1/happy.c
+++ hw/hw_1/happy.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	int n;
-	printf("n = ");
-	scanf("%d", &n);
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
 
-	int m = n;
+    int m = n;
     int x;
     int ones;
     while ((n != 4) && (n != 1)) {
@@ -19,11 +19,11 @@ int main() {
         printf("%d\n", n);
     }
 
-	if (n == 1) {
-		printf("%d is a happy number.\n", m);
-	}
-	else {
-		printf("%d is NOT a happy number.\n", m);
-	}
-	return 0;
+    if (n == 1) {
+        printf("%d is a happy number.\n", m);
+    }
+    else {
+        printf("%d is NOT a happy number.\n", m);
+    }
+    return 0;
 }
