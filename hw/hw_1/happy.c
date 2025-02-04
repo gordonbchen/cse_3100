f0c04bdef2b0b663d6a1a184b97bfca8071141a3
gordonbchen <gordonbchen@gmail.com>
2025-02-04 11:47:00 -0500

    add hw 1 starter

---
+++ hw/hw_1/happy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("n = ");
+	scanf("%d", &n);
+
+	int m = n;
+
+	if (n == 1) {
+		printf("%d is a happy number.\n", m);
+	}
+	else {
+		printf("%d is NOT a happy number.\n", m);
+	}
+	return 0;
+}
---
+++ hw/hw_1/pi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
+
+    double pi = 0.0;
+
+    printf("PI = %.10f\n", pi);
+    return 0;
+}
