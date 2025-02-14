57e179fcf5bc0c427c39bac05821005c3bd15fcb
gordonbchen <gordonbchen@gmail.com>
2025-02-14 17:12:07 -0500

    add lab3 part 2

--- labs/lab_3/argvcat.c
+++ labs/lab_3/argvcat.c
@@ -15,21 +15,31 @@ void my_error(char *s)
  */
 char *my_strcat(char *s1, char *s2)
 {
-    // TODO 
-    return NULL;
+    int len = strlen(s1) + strlen(s2) + 1;
+    char* s3 = (char*) malloc(len * sizeof(char));
+    if (s3 == NULL) {
+        my_error("Failed to malloc.\n");
+    }
+    s3[0] = '\0';
+    s3 = strcat(s3, s1);
+    s3 = strcat(s3, s2);
+    return s3;
 }
 
 int main(int argc, char *argv[])
 {
     char *s;
-
+    char *prev;
     s = my_strcat("", argv[0]);
 
-    for (int i = 1; i < argc; i ++) {
-        s = my_strcat(s, argv[i]);
+    for (int i = 1; i < argc; i++) {
+        prev = s;
+        s = my_strcat(prev, argv[i]);
+        free(prev);
     }
 
     printf("%s\n", s);
 
+    free(s);
     return 0;
 }
