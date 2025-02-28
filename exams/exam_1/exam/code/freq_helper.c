f5ae8568859fd685b09b3b30bbddc819f485d5bf
gordonbchen <gordonbchen@gmail.com>
2025-02-28 14:23:13 -0500

    add exam 1 starter

---
+++ exams/exam_1/exam/code/freq_helper.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+
+//TODO
+unsigned firstDigit(unsigned k)
+{
+	//fill in code below
+
+}
+
+//TODO
+void FirstDigitCount(unsigned *a, unsigned n, unsigned freq[10])
+{
+	//fill in your code below
+
+}
+
