c9b628cbde64e0726e6b82342ffba16742914a61
gordonbchen <gordonbchen@gmail.com>
2025-02-28 18:55:37 -0500

    add exam 1 part 2

--- exams/exam_1/exam/linked_list.c
+++ exams/exam_1/exam/linked_list.c
@@ -1,47 +1,47 @@
 #include "linked_list.h"
 
-//create a node with value v
-node * create_node(int v) 
+// create a node with value v
+node *create_node(int v)
 {
-    node	* p = malloc(sizeof(node)); // Allocate memory
-    assert(p != NULL);		// you can be nicer
-    
-    // Set the value in the node. 
-    p->v = v;		
-    p->next = NULL;  
-    return p;			// return
+	node *p = malloc(sizeof(node)); // Allocate memory
+	assert(p != NULL);				// you can be nicer
+
+	// Set the value in the node.
+	p->v = v;
+	p->next = NULL;
+	return p; // return
 }
 
-//TODO:
+// TODO:
 void insert(node **head, node *newnode)
 {
-    if((*head) == NULL)
-    {
-		//fill in code below
-
-    }
-    node *p = (*head);
+	if ((*head) == NULL)
+	{
+		*head = newnode;
+		return;
+	}
+	node *p = (*head);
 	node *q = p;
-	while(p->next != NULL && newnode->v > p->v)
+	while (p->next != NULL && newnode->v > p->v)
 	{
-		//fill in code below
-
+		q = p;
+		p = p->next;
 	}
-	if(newnode->v > p->v)
+	if (newnode->v > p->v)
 	{
-		// fill in code below
-
+		p->next = newnode;
+		return;
 	}
-	//fill in code below
-
+	q->next = newnode;
+	newnode->next = p;
 	return;
 }
 
-//add newnode the last of the linked list determined by *head and *tail
-//note **head, **tail
+// add newnode the last of the linked list determined by *head and *tail
+// note **head, **tail
 void insert_last(node **head, node **tail, node *newnode)
 {
-	if((*head) == NULL)
+	if ((*head) == NULL)
 	{
 		(*head) = (*tail) = newnode;
 	}
@@ -52,25 +52,26 @@ void insert_last(node **head, node **tail, node *newnode)
 	}
 }
 
-//remove the first node from the list
-//note **head
-//return a pointer to the removed content
+// remove the first node from the list
+// note **head
+// return a pointer to the removed content
 
-node * remove_first(node **head) 
+node *remove_first(node **head)
 {
-        node *p;
+	node *p;
 
-        p = (*head);
-        if((*head)!=NULL) (*head) = (*head)->next;
-        return p;
+	p = (*head);
+	if ((*head) != NULL)
+		(*head) = (*head)->next;
+	return p;
 }
 
-//print the list pointed by head
+// print the list pointed by head
 void print_list(node *head)
 {
 	node *p = head;
 
-	while(p!=NULL)
+	while (p != NULL)
 	{
 		printf("%d ", p->v);
 		p = p->next;
@@ -78,83 +79,97 @@ void print_list(node *head)
 	printf("\n");
 }
 
-//TODO
-//merge two sorted linked list as one
-node * merge(node **p1, node **p2)
+// TODO
+// merge two sorted linked list as one
+node *merge(node **p1, node **p2)
 {
 	node *head = NULL, *tail = NULL;
 	node *q1 = (*p1), *q2 = (*p2), *p;
 
-	if(q1 == NULL && q2 == NULL) return NULL;
-	if(q1 != NULL && q2 == NULL) 
+	if (q1 == NULL && q2 == NULL)
+		return NULL;
+	if (q1 != NULL && q2 == NULL)
 	{
 		p = q1;
 		(*p1) = NULL;
 		return p;
 	}
-	if(q1 == NULL && q2 != NULL) 
+	if (q1 == NULL && q2 != NULL)
 	{
 		p = q2;
 		(*p2) = NULL;
 		return p;
 	}
 
-	while(q1 != NULL && q2!= NULL)
+	while (q1 != NULL && q2 != NULL)
 	{
-		//fill in code below
-
+		if (q1->v < q2->v) {
+			insert_last(&head, &tail, q1);
+			q1 = q1->next;
+			tail->next = NULL;
+		}
+		else {
+			insert_last(&head, &tail, q2);
+			q2 = q2->next;
+			tail->next = NULL;
+		}
 	}
-	while(q1 != NULL)
+	while (q1 != NULL)
 	{
-		//fill in code below
-
+		insert_last(&head, &tail, q1);
+		q1 = q1->next;
+		tail->next = NULL;
 	}
-	while(q2 != NULL)
-	{	
-		//fill in code below
-	
+	while (q2 != NULL)
+	{
+		insert_last(&head, &tail, q2);
+		q2 = q2->next;
+		tail->next = NULL;
 	}
-	//fill in code below
-
-	return 	head;
+	*p1 = NULL;
+	*p2 = NULL;
+	return head;
 }
 int main(int argc, char *argv[])
 {
 	assert(argc == 2);
 	int n = atoi(argv[1]);
-	
+
 	int a[n];
 	int b[n];
-	
-	for(int i = 0; i < n; i++)
+
+	for (int i = 0; i < n; i++)
 	{
 		a[i] = i;
-		b[i] = 2*i;
+		b[i] = 2 * i;
 	}
 
 	node *p1 = NULL, *p2 = NULL;
 
-	for(int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		node *p = create_node(a[i]);
- 		insert(&p1, p);
+		insert(&p1, p);
 	}
 	print_list(p1);
 
-	for(int i = 0; i < n; i++)
-    {
-        node *p = create_node(b[i]);
-        insert(&p2, p);
-    }
+	for (int i = 0; i < n; i++)
+	{
+		node *p = create_node(b[i]);
+		insert(&p2, p);
+	}
 	print_list(p2);
-	
+
 	node *p = merge(&p1, &p2);
 	print_list(p);
 	printf("%p\n", p1);
 	printf("%p\n", p2);
 
-	//TODO
-	//fill in code below
-
+	node* temp;
+	while (p != NULL) {
+		temp = p->next;
+		free(p);
+		p = temp;
+	}
 	return 0;
 }
